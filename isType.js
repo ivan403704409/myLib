30b98778c6a74ed14b523c639385b361720e17ab
ivan403704409 <403704409@qq.com>
2015-02-06 21:57:44 +0800

    type

---
+++ isType.js
@@ -0,0 +1,18 @@
+//类型判断
+function isType (type) {
+	return function (obj){
+		return Object.prototype.toString.call(obj) == '[object '+ type +']';
+	}
+}
+var isNull = isType('Null');
+var isUndefined = isType('Undefined');
+var isBoolean = isType('Boolean');
+var isNumber = isType('Number');
+var isString = isType('String');
+var isArray = isType('Array');
+var isObject = isType('Object');
+var isFunction = isType('Function');
+var isNaN = function(obj){
+	return typeof obj==='number'&& obj!==obj ? true : false;
+}
+
